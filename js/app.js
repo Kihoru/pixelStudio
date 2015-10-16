c934db0c1d6955ca410dbd111a85d45ebe221cf8
Thomas Rigal <thomas.graph.rigal@gmail.com>
2015-10-16 14:51:51 +0200

    pxst v0.03

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 var pixelStudio = {
 
 	init: function() {
-		console.log('pixelStudio ready to rock!');
+		
 	}
 };
+
+
+		
--- js/pixel.class.js
+++ js/pixel.class.js
@@ -1,9 +1,49 @@
-function pixel (size, color) {
-	var size = size || '50';
-	var color = color || 'white';
-	var empty = true;
+/**
+	* Pixel représente un pixel dans le cadre de dessin
+	* @param {number} x 		position horizontale
+	* @param {number} y 		position verticale
+	* @param {number} size 		taille du pixel
+	* @param {string} color 	couleur du pixel
+**/
+
+function Pixel (size, color, posX, posY) {
+	this.$el = $('<div class="pixel"></div>');
+	this.set_color(color || 'red');
+	this.set_size(size || 50);
+	this.set_position(posX, posY);
+
+	/*$('#drawArea').append(this.$el);*/
 }
 
-pixel.prototype = {
+Pixel.prototype = {
+	/**
+	* @param {number} size 		taille du pixel
+	**/
+	set_size: function(size) {
+		this.size = size;
+		this.$el.css({
+			width 				: this.size+'px',
+			height				: this.size+'px',
+		});
+	},
+	/**
+	* @param {number} x 		position horizontale
+	* @param {number} y 		position verticale
+	**/
+	set_position: function(posX, posY) {
+		this.posX = posX;
+		this.posY = posY;
+		this.$el.css({
+			left	: (this.posX - 1) * this.size + 'px',
+			top 	: (this.posY - 1) * this.size + 'px',
+		});
+	},
+	/**
+	* @param {string} color 	couleur du pixel
+	**/
+	set_color: function(color) {
+		this.color = color;
+		this.$el.css('background-color', this.color);
+	},
 	
 };
